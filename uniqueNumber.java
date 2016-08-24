1bc801d763c525ad177905e2178a83e8b561b706
sindhuela <sindhuela95@gmail.com>
2016-08-24 22:22:32 +0530

    Update uniqueNumber.java

--- uniqueNumber.java
+++ uniqueNumber.java
@@ -3,6 +3,7 @@ public class uniqueNumber {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		int siz=input.nextInt();
+		if(siz>0){
 		int count=0;
 		int[] arr=new int[siz];
 		for(int i=0;i<siz;i++)
@@ -15,8 +16,11 @@ public class uniqueNumber {
 					arr[j]=0;
 				}
 		if(count==0)
-			System.out.println(arr[i]);
-		}
+			System.out.print(arr[i]);
+		}}
+		else
+		System.out.print("Error");
+		
 	}
 
 }
