3f531305a532f7ac73229698f48aa3ba41032b29
sindhuela <sindhuela95@gmail.com>
2016-08-24 22:32:20 +0530

    Create firstRepeatingNumber.java

---
+++ firstRepeatingNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class firstRepeatingNumber {
+	public static void main(String[] args) {
+			Scanner input=new Scanner(System.in);
+			int siz=input.nextInt();
+			int count=0;
+			int[] arr=new int[siz];
+			for(int i=0;i<siz;i++)
+				arr[i]=input.nextInt();
+			for(int i=0;i<siz-1;i++)
+				for(int j=i+1;j<siz;j++)
+					if(arr[i]==arr[j])
+					{
+						System.out.println(arr[i]);
+						System.exit(0);
+					}
+	}
+
+}
