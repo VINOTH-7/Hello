107a85f1462d9387851ac65f47e48a91da10d601
sindhuela <sindhuela95@gmail.com>
2016-08-24 18:31:07 +0530

    Create findingRepeatedNumbers.java

---
+++ findingRepeatedNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class findingRepeatedNumbers {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int siz=input.nextInt();
+		int[] count=new int[siz];
+		int[] arr=new int[siz];
+		for(int i=0;i<siz;i++)
+			arr[i]=input.nextInt();
+		for(int i=0;i<siz-1;i++)
+			for(int j=i+1;j<siz;j++)
+				if(arr[i]==arr[j])
+					count[i]++;
+		for(int i=0;i<siz;i++)
+			if(count[i]>0)
+				System.out.print(arr[i]+" ");
+	}
+}
